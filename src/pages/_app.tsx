fdb2ebe4175fdca68421dc7ccf3415dac2ac5f25
unknown <arvinciu86@yahoo.com>
2023-04-29 00:25:39 +0700

    fix:metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,23 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
+    <Head>
+      <title>Kelompok Inggris</title>
+      <meta charSet="utf-8" />
+      <meta property="og:title" content='Kelompok Inggris'/>
+      <meta property="og:site_name" content='Kelompok Inggris'/>
+      <meta property="og:description" content='Consists of not just anyone, but extraordinary people'/>
+      <meta property="og:url" content="kelompok-inggris-web.vercel.app"/>
+      <meta
+        property="og:image"
+        content="https://kelompok-inggris-web.vercel.app/images/KPI_LOGO-03.png"
+      />
+    </Head>
     <Component {...pageProps} />
     <ToastContainer newestOnTop/>
   </>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,18 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Kelompok Inggris</title>
-        <meta charSet="utf-8" />
-        <meta property="og:title" content='Kelompok Inggris'/>
-        <meta property="og:site_name" content='Kelompok Inggris'/>
-        <meta property="og:description" content='Consists of not just anyone, but extraordinary people'/>
-        <meta property="og:url" content="kelompok-inggris-web.vercel.app"/>
-        <meta
-          property="og:image"
-          content="https://kelompok-inggris-web.vercel.app/images/KPI_LOGO-03.png"
-        />
-      </Head>
+      <Head/>
       <body>
         <Main />
         <NextScript />
