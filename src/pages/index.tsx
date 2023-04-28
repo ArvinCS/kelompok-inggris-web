064f9e8beb666dba9e2392c3eca10d185dfef18b
unknown <arvinciu86@yahoo.com>
2023-04-28 17:15:25 +0700

    feat:remake the web with typescript and tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,116 +3,197 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+import localFont from "next/font/local"
+
+const memberData = [
+  {
+    id: 1,
+    title: "Henson Divenso",
+    image: "/images/henson.jpg",
+  },
+  {
+    id: 2,
+    title: "Arvin",
+    image: "/images/arvin.jpg"
+  },
+  {
+    id: 3,
+    title: "William",
+    image: "/images/william.jpg"
+  },
+  {
+    id: 4,
+    title: "Purnata Raharja",
+    image: "/images/purnata.jpg",
+  },
+  {
+    id: 5,
+    title: "Wilsen",
+    image: "/images/wilsen.jpg"
+  },
+  {
+    id: 6,
+    title: "Fery Raymond",
+    image: "/images/fery.jpg"
+  },
+  {
+    id: 7,
+    title: "Handy Wijaya Sutanto",
+    image: "/images/handy.jpg"
+  },
+  {
+    id: 12,
+    title: "Valent",
+    image: "/images/valent.png"
+  },
+  {
+    id: 13,
+    title: "Hansen Wijaya",
+    image: "/images/hansen.jpg"
+  },
+  {
+    id: 9,
+    title: "Joan Justian",
+    image: "/images/joan.jpg"
+  },
+  {
+    id: 8,
+    title: "Abel Timothy Leonard",
+    image: "/images/placeholder.jpg"
+  },
+  {
+    id: 10,
+    title: "Valentino",
+    image: "/images/placeholder.jpg"
+  },
+  {
+    id: 11,
+    title: "Steven Bryan",
+    image: "/images/placeholder.jpg"
+  },
+];
+
+const clashDisplay = localFont({
+  src: [
+    {
+      path: '../../public/fonts/ClashDisplay/ClashDisplay-Extralight.ttf',
+      weight: '200'
+    },
+    {
+      path: '../../public/fonts/ClashDisplay/ClashDisplay-Light.ttf',
+      weight: '300'
+    },
+    {
+      path: '../../public/fonts/ClashDisplay/ClashDisplay-Regular.ttf',
+      weight: '400'
+    },
+    {
+      path: '../../public/fonts/ClashDisplay/ClashDisplay-Medium.ttf',
+      weight: '500'
+    },
+    {
+      path: '../../public/fonts/ClashDisplay/ClashDisplay-SemiBold.ttf',
+      weight: '600'
+    },
+    {
+      path: '../../public/fonts/ClashDisplay/ClashDisplay-Bold.ttf',
+      weight: '700'
+    },
+  ],
+  variable: '--font-clashdisplay'
+})
+
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between ${clashDisplay.variable} font-clashdisplay`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className='flex h-screen w-full bg-green-50 justify-between'>
+        <div className='z-10 flex flex-row absolute items-center h-[40px] w-full bg-black justify-between px-4'>
+          <h2 className='flex my-auto font-clashdisplay font-bold'>
+            Kelompok Inggris
+          </h2>
+          <div className='flex flex-row space-x-4'>
+            <h2>  
+              About Us
+            </h2>
+            <h2>  
+              Members
+            </h2>
+            <h2>  
+              Memories
+            </h2>
+          </div>
+        </div>
+        <div className='flex absolute z-0 min-w-full min-h-full bg-blue-800 overflow-hidden'>
+          <video
+            autoPlay
+            loop
+            muted
+            className='flex min-w-full w-[200%] object-fill min-h-full brightness-50 overflow-hidden'
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <source src='/video.mp4' type='video/mp4'/>
+          </video>
+        </div>
+        <div className='flex m-auto items-center flex-col'>
+          <h2 className='z-20 text-[4vw] font-medium'>
+            Kelompok Inggris
+          </h2>
+          <h2 className='z-20 text-[10px] md:text-[16px]'>
+            Consists of not just anyone, but extraordinary people
+          </h2>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {/* <div className='flex h-screen w-full bg-white'>
+        test
+      </div> */}
+      <MemberPage/>
     </main>
   )
+}
+
+export const MemberPage = () => {
+  return (
+    <div id="MemberList" className='flex items-center flex-col relative h-screen w-full bg-black justify-between'>
+      <p className="font-bold text-4xl h-auto p-5">Our Members</p>
+      <div className="grid grid-cols-2 md:grid-cols-3 relative h-full w-full">
+        {
+          memberData.map(item => (
+            <MemberTile
+              key={item.id}
+              name={item.title}
+              photoUrl={item.image}
+            />
+          ))
+        }
+      </div>
+    </div>
+  );
+} 
+
+export const MemberTile = ({name, photoUrl} : {name: string, photoUrl: string}) => {
+  return (
+    <>
+      <div className="flex flex-col relative m-5 justify-center place-items-center">
+        <div className='relative aspect-[9/16] items-center w-3/4 md:w-2/3'>
+          <Image 
+            src={photoUrl}
+            alt={''}
+            className='rounded-2xl'
+            fill={true}
+            priority={true}
+          />
+        </div>
+        <div className="relative text-white font-bold bg-black px-[2px] py-[10px]">
+          {name}
+          {/* <p style={{
+            'color': 'white',
+            'fontWeight': 'bold',
+            'backgroundColor': 'black',
+            'padding': '2px 10px',
+          }}>{name}</p>  */}
+        </div>
+      </div>
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        clashdisplay: ['var(--font-clashdisplay)']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
